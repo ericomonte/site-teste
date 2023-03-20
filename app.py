36c45bc71384db85d4abf9ca5de4409dbf0a4dee
ericomonte <114265979+ericomonte@users.noreply.github.com>
2023-03-20 18:57:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">Página inicial</a>  | <a href="/sobre">Sobre</a>  | <a href="/contato">Contato</a>
+"""
+
 @app.route("/")
 def hello_world():
-  return "Olá, mundo! Esse é meu site. (Érico Monte)"
+  return menu + "Olá, mundo! Esse é meu site. (Érico Monte)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da página sobre"
+  return menu + "Aqui vai o conteúdo da página sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da página contato"
+  return menu + "Aqui vai o conteúdo da página contato"
