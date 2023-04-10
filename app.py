da94196249fb74f2bf190c6d133b5d59fd3044f3
ericomonte <114265979+ericomonte@users.noreply.github.com>
2023-04-10 11:50:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
 import os
 
+import datetime
+import gspread
+import pandas as pd
 import requests
-from flask import Flask
-from tchan import ChannelScraper
 
+from flask import Flask
+from haversine import haversine
+from oauth2client.service_account import ServiceAccountCredentials
+from unidecode import unidecode
 
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
-
+GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+with open("credenciais.json", mode="w") as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+api = gspread.authorize(conta) # sheets.new
+planilha = api.open_by_key("1RAvxiZG3f-WhfnX2I7XgyB6TKhyVxuJT5vVAMzO45_U")
+sheet_municipios = planilha.worksheet("municipios")
+sheet = planilha.worksheet("updates")
 
 app = Flask(__name__)
 
 
 
-def ultimas_promocoes():
-  scraper = ChannelScraper()
-  contador = 0
-  resultado = []
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    resultado.append(f"{message.created_at} {texto}")
-    if contador == 10:
-      return resultado
 
-    
+
+
+
+
+
+
+
+
+
 menu = """
 <a href="/">Página inicial</a>  | <a href="/promocoes">PROMOÇÕES</a> |  <a href="/sobre">Sobre</a>  | <a href="/contato">Contato</a><br>
 """
@@ -61,3 +71,9 @@ def dedoduro():
   resposta = requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
   return f"Mensagem enviada. Resposta ({resposta.status_code}): {resposta.text}"
   
+@app.route("/dedoduro2")
+def dedoduro2():
+  sheet.append_row(["Érico", "Monte", "a partir do Flask"])
+  return "Planilha escrita!"
+
+
