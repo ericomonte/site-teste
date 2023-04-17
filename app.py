8a9c4b373b40ec2e929bd4f485131b1d3b8feef8
ericomonte <114265979+ericomonte@users.noreply.github.com>
2023-04-17 14:40:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 
 import gspread
+import pandas as pd
 import requests
-from flask import Flask, request
-from oauth2client.service_account import ServiceAccountCredentials
 
+from flask import Flask, request
+from haversine import haversine
+from oauth2client.service_account import ServiceAccountCredentials
+from unidecode import unidecode
 
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
@@ -16,7 +19,70 @@ api = gspread.authorize(conta)
 planilha = api.open_by_key("1RAvxiZG3f-WhfnX2I7XgyB6TKhyVxuJT5vVAMzO45_U")
 sheet = planilha.worksheet("Sheet1")
 app = Flask(__name__)
+
+@app.route("/telegram-bot", methods=["POST"])
+def telegram_bot():
+  update = request.json
+  chat_id = update["message"]["chat"]["id"]
+  message = update["message"]["text"]
+
+  if message == "/start":
+    texto_resposta = "Olá! Seja bem-vinda(o). Se você chegou aqui está preocupado com o avanço dos incêndios florestais. Envie o nome de sua cidade para saber se está próximo a focos de incêndio:"
+  
+  else:
+    # recebe a cidade pelo bot do telegram retira acentos e transforma em caixa baixa
+    message = unidecode(cidade_bot)
+    message = cidade_bot = cidade_bot.lower()
     
+    # procura a cidade na planilha do sheets onde consta a base de municipios do IBGE + respectivas coordenadas de latitude e longitude
+    cell = sheet_municipios.find(message)
+    
+    # obtém as coordenadas da célula encontrada e caprura os dados nas colunas à direita
+    row = cell.row
+    col = cell.col
+    latitude = sheet_municipios.cell(row, col+1).value
+    longitude = sheet_municipios.cell(row, col+2).value
+    message = (float(latitude), float(longitude))
+    
+    # acessa a api do Programa Queimadas do INPE com os dados de novos focos de incêndio nas últimas 48h
+    inpe_focos48h_url = 'https://queimadas.dgi.inpe.br/home/download?id=focos_brasil&time=48h&outputFormat=csv&utm_source=landing-page&utm_medium=landing-page&utm_campaign=dados-abertos&utm_content=focos_brasil_48h'
+    foco_atual = pd.read_csv(inpe_focos48h_url)
+    
+    # padroniza em caixa alta e remove acentos
+    foco_atual['municipio'] = [x.upper() for x in foco_atual['municipio']]
+    foco_atual['nome'] = foco_atual['municipio'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+    
+    # captura as cordenadas dos focos na base do INPE, cruza com as coordenadas da cidade indicada pelo usuário, calculando a distância entre os pontos. Seleciona a menor distância e retorna o valor.
+    coordenadas = []
+    distancia = []
+    for x, y in zip(foco_atual.latitude.values, foco_atual.longitude.values):
+      lat_long = (x,y)
+      coordenadas.append(lat_long)
+    for n in coordenadas:
+      km = haversine(cidade_bot_coord, n)
+      distancia.append(km)
+    foco_atual['distancia_km'] = distancia
+    foco_incendio = int(foco_atual['distancia_km'].min())
+    
+    texto_resposta = (f"O foco de incêndio mais próximo, detectado pelo Inpe nas últimas 48h, encontra-se a {foco_incendio}km de você.")
+    
+    #elif:
+      
+    
+    #texto_resposta = "Olá, não entendi o que você quis dizer. Se você chegou aqui está preocupado com o avanço dos incêndios florestais. Envie o nome de sua cidade para saber se está próximo a focos de incêndio:"
+  
+  nova_mensagem = {
+    "chat_id": chat_id,
+    "text": texto_resposta,
+    "parse_mode": "HTML",
+  }  
+  
+  resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+  print(resposta.text)
+  return "ok"
+
+
+
 menu = """
 <a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
@@ -25,14 +91,6 @@ menu = """
 @app.route("/")
 def index():
   return menu + "Olá, mundo! Esse é meu site. (Érico Monte)"
-
-@app.route("/sobre")
-def sobre():
-  return menu + "Aqui vai o conteúdo da página Sobre"
-
-@app.route("/contato")
-def contato():
-  return menu + "Aqui vai o conteúdo da página Contato"
 
 
 @app.route("/dedoduro")
@@ -46,41 +104,3 @@ def dedoduro():
 def dedoduro2():
   sheet.append_row(["xx", "xx", "a partir do Flask"])
   return "Planilha escrita!"
-
-
-@app.route("/telegram-bot", methods=["POST"])
-def telegram_bot():
-  update = request.json
-  chat_id = update["message"]["chat"]["id"]
-  message = update["message"]["text"]
-
-  if message == "/start":
-    texto_resposta = "Olá! Seja bem-vinda(o). Se você chegou aqui está preocupado com o avanço dos incêndios florestais. Envie o nome de sua cidade para saber se está próximo a focos de incêndio:"
-  
-  else:
-    texto_resposta = "Resposta errada."
-  
-  nova_mensagem = {
-    "chat_id": chat_id,
-    "text": texto_resposta,
-    "parse_mode": "HTML",
-  }  
-  
-  resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  print(resposta.text)
-  return "ok"
-
-
-def enviar_foto():
-  # Esboço
-  ...
-  chart.save("chart.png")
-  with open("chart.png", mode="rb") as arquivo:
-    conteudo = arquivo.read()
-  mensagem = {"chat_id": ..., "caption": "gráfico X"}
-  files = {"photo": ("grafico.png", conteudo)}
-  resposta = requests.post(
-    f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendPhoto", 
-    data=mensagem, 
-    files=files,
-  )
